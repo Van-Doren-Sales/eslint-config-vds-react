54089c7615c4484e6636b392db627b69253d0d22
Chris S <13677134+devcshort@users.noreply.github.com>
2021-03-12 11:04:11 -0800

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:import/errors',
+    'plugin:react/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:prettier/recommended',
+    'prettier/react',
+  ],
+  plugins: ['react-hooks'],
+  rules: {
+    'react/prefer-stateless-function': 'off',
+    'react/prop-types': 0,
+    'react-hooks/exhaustive-deps': 'warn',
+    'no-irregular-whitespace': 'off',
+    indent: ['error', 2, { switchCase: 1 }],
+    'linebreak-style': ['error', 'windows'],
+    semi: ['error', 'always'],
+    'eol-last': ['error', 'always'],
+    quotes: ['error', 'single'],
+    'prefer-const': [
+      'error',
+      {
+        destructuring: 'any',
+        ignoreReadBeforeAssign: false,
+      },
+    ],
+  },
+};
